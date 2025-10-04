5b9fd226e4968b8ca3fbe1d87cd76381ec6aa458
Sertavr <pampuh.net@gmail.com>
2025-10-04 20:27:33 +0300

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,16 +5,38 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
+    if (
+      compareFunction !== undefined &&
+      typeof compareFunction !== 'function'
+    ) {
+      throw new TypeError(
+        'The comparison function must be either a function or undefined',
+      );
+    }
+
     for (let i = 0; i < this.length; i++) {
       for (let j = i + 1; j < this.length; j++) {
         const a = this[i];
         const b = this[j];
 
-        const comparison = compareFunction
-          ? compareFunction(a, b)
-          : String(a) > String(b)
-            ? 1
-            : 0;
+        let comparison;
+
+        if (a === undefined) {
+          comparison = 1;
+        }
+
+        if (b === undefined) {
+          comparison = -1;
+        }
+
+        if (compareFunction) {
+          comparison = compareFunction(a, b);
+        } else {
+          const aStr = String(a);
+          const bStr = String(b);
+
+          comparison = aStr > bStr ? 1 : aStr < bStr ? -1 : 0;
+        }
 
         if (comparison > 0) {
           [this[i], this[j]] = [this[j], this[i]];
